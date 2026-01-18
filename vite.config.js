001d2d834f752bbfba6ecd47727f46636b287f25
James Fu <126029317+JamesCFu@users.noreply.github.com>
2026-01-17 22:38:11 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // <--- THIS IS THE KEY FIX. It makes paths relative.
+  base: './', 
 })
