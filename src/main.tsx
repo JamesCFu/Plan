a18e9f78ea6af54b428d19b0ac5df6b4c03a8395
James Fu <126029317+JamesCFu@users.noreply.github.com>
2026-01-17 22:27:52 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,7 @@
-
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css' 
+import App from './App' // points to App.tsx in same folder
+import './index.css'    // points to index.css in same folder
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
